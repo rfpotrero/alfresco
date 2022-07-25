abb5c00231833823d8daae70df23c596e5940380
rfpotrero <raulf.potrero@gmail.com>
2022-07-25 12:57:01 +0000

    Add runtime file to resolve backports.zoneinfo
    Adding runtime.txt to resolve issue while deploying
    in heroku. #6

--- alfresco/settings.py
+++ alfresco/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["alfresco.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["alfresco.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
