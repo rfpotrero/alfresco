0b87e4a6f60801e975e4168d03ace474cf0b316d
rfpotrero <raulf.potrero@gmail.com>
2022-07-31 14:27:06 +0000

    Add views to website/views.py
    Adding functions to render the view templates. Track in issue #8

--- website/views.py
+++ website/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def get_home(request):
     return render(request, '../templates/home.html')
+
+    
+def get_menu(request):
+    return render(request, '../templates/menu.html')
+
+
+def get_contact(request):
+    return render(request, '../templates/contact.html')
