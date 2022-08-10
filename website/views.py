4b76217b636fefa69ae27331109f4b2e51f6b621
rfpotrero <raulf.potrero@gmail.com>
2022-08-10 11:39:03 +0000

    Add reservations template to views
    Ading reservations template to views.
    Track in #8

--- website/views.py
+++ website/views.py
@@ -4,10 +4,14 @@ from django.shortcuts import render
 def get_home(request):
     return render(request, '../templates/home.html')
 
-    
+
 def get_menu(request):
     return render(request, '../templates/menu.html')
 
 
 def get_contact(request):
     return render(request, '../templates/contact.html')
+
+
+def get_reservations(request):
+    return render(request, '../templates/reservations.html')
