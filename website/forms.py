92b3097df53f18c65255698befa75ee21f4bdab8
rfpotrero <raulf.potrero@gmail.com>
2022-08-23 14:14:57 +0000

    Add forms.py file to capture reservations
    Adding forms.py form to use the reservations model as template
    to catpure users reservations. Tracked in #10

---
+++ website/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import Reservations
+
+class ReservationForm(ModelForm):
+    class Meta:
+        model = Reservations
+        fields = ('date', 'name', 'phone', 'email', 'numberofpeople')
+
+
+form = ReservationForm()
+
