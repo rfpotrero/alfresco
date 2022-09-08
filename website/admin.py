6c58b8efbff9818e8023d012368ddd4a871b168c
rfpotrero <raulf.potrero@gmail.com>
2022-09-08 14:45:24 +0000

    Edit admin view to display further fields
    Add further fields in admin view to be able to identify bookings.
    Tracked in #20

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Reservations
 
-# Register your models here.
-admin.site.register(Reservations)
+#admin.site.register(Reservations)
+
+@admin.register(Reservations)
+class ReservationsAdmin(admin.ModelAdmin):
+    list_display = ('date', 'name', 'phone', 'reservation_code')
+    ordering = ('date',)
