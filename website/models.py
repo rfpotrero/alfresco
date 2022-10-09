24448f5ade88abcdd7198404e9e6e376a26b5f8d
rfpotrero <raulf.potrero@gmail.com>
2022-10-09 13:07:42 +0000

    Change website models.py
    Updating models to represented changes after mentor talk. I will
    include a client registration page and thus the models needs to
    totally change. #51

--- website/models.py
+++ website/models.py
@@ -8,22 +8,35 @@ class Reservations(models.Model):
     """
     Model for client reservations
     """
-    date = models.DateTimeField()
-    name = models.CharField(max_length=100)
-    phone = PhoneNumberField()
-    email = models.EmailField()
+    date_reservation = models.DateField()
+    time_reservation = models.TimeField()
+    client = models.ForeignKey(
+        Client, related_name="client")
     numberofpeople = models.IntegerField(default=2)
     reservation_code = ShortUUIDField(
         length=7,
         max_length=10,
         alphabet="abcdefg1234",
         editable=False)
-    reservation_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                                      editable=False)
 
+        
     class Meta:
         ordering = ['-date']
         verbose_name_plural = "Reservations"
 
     def __str__(self):
         return self.name
+
+class Client(models.Model):
+    """
+    Moldel to store new clients
+    """
+    client_id = models.AutoField(primary_key=True)
+    name_client = models.CharField(max_length=100)
+    email_client = models.EmailField(unique=True, max_length=200)
+    phonenumber_client = phone = PhoneNumberField()
+    
+
+    def __str__(self):
+        return self.name_client
+    
