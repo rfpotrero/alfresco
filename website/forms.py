db85db22dbcc05051e56f71b29aa9c1a3bccb8ab
rfpotrero <raulf.potrero@gmail.com>
2022-10-11 19:45:28 +0000

    Updating forms.py to capture the logged user
    Changing forms.py to reflect new changes in model and workflow
    the form will capture the logged user to associate the reservation
    when the forms is submitted. #57

--- website/forms.py
+++ website/forms.py
@@ -5,15 +5,16 @@ from .models import Reservations
 class ReservationsForm(forms.ModelForm):
     class Meta:
         model = Reservations
-        fields = ('date_reservation', 'time_reservation', 'numberofpeople')
+        exclude = ('client', 'reservation_code')
         widgets = {
+
             'date_reservation': forms.DateInput(attrs={
                                                 'class': 'form-control',
                                                 'type': 'date'}),
             'time_reservation': forms.Select(attrs={'class': 'form-control'}),
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'name_client': forms.TextInput(attrs={'class': 'form-control'}),
+            'phonenumber_client': forms.TextInput(attrs={'class': 'form-control'}),
+            'email_client': forms.EmailInput(attrs={'class': 'form-control'}),
             'numberofpeople': forms.Select(attrs={'class':
                                                   'form-control'})
         }
