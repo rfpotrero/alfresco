9638c68998f2deb8b98b0c269609f0230f778189
rfpotrero <raulf.potrero@gmail.com>
2022-10-18 11:17:38 +0000

    Change phone number time input to tel
    Changing phone number field type to Tel to help with input validation
    The validation will display now when a phone number is not correct.
    If phone number is not in Irish national formar will ask for a new
    number or a international number. #57

--- website/forms.py
+++ website/forms.py
@@ -13,7 +13,8 @@ class ReservationsForm(forms.ModelForm):
                                                 'type': 'date'}),
             'time_reservation': forms.Select(attrs={'class': 'form-control'}),
             'name_client': forms.TextInput(attrs={'class': 'form-control'}),
-            'phonenumber_client': forms.TextInput(attrs={'class': 'form-control'}),
+            'phonenumber_client': forms.TextInput(attrs={'class': 'form-control',
+                                                        'type' : 'tel'}),
             'email_client': forms.EmailInput(attrs={'class': 'form-control'}),
             'numberofpeople': forms.Select(attrs={'class':
                                                   'form-control'})
