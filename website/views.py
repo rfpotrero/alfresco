fe8803024659b59037550fcd72624daad0637ce5
rfpotrero <raulf.potrero@gmail.com>
2022-10-18 11:21:30 +0000

    Create function to validate duplicate reservations
    Move validation to a separate function to be used in make reservations
    and update reservations. #59

--- website/views.py
+++ website/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from .models import Reservations
 from .forms import ReservationsForm
 
+
 def home(request):
     return render(request, '../templates/home.html')
 
@@ -25,30 +26,29 @@ def make_reservation(request):
     """
     if request.method == "POST":
         form = ReservationsForm(request.POST)
-        # Double reservation verification based in User logged, time and date.
         if form.is_valid():
             time = request.POST.get('time_reservation')
             date = request.POST.get('date_reservation')
             form.instance.client = request.user
-            duplicate_reservations = Reservations.objects.filter(
-                Q(time_reservation=time),
-                Q(date_reservation=date),
-                Q(client=request.user)
-            )
+            # Double reservation verification based in User logged, time and date.
+            duplicate_reservations = duplicate_reservations_verification(time, date, form)
             if len(duplicate_reservations) > 0:
                 messages.error(request, 'There is an existing reservation')
                 return render(request, '../templates/reservations.html',
-                             {'form': form})
+                                       {'form': form})
             form.save()
             form_data = form.save()
             return render(request, '../templates/confirmed.html',
-                                {'form_data': form_data})
-    else:
-        form = ReservationsForm
+                                   {'form_data': form_data})
+
         return render(request, '../templates/reservations.html',
                                {'form': form})
 
+    form = ReservationsForm
+    return render(request, '../templates/reservations.html',
+                            {'form': form})
 
+@login_required
 def get_search_reservation(request):
     """
     Search for existing reservation using the reservation code.
@@ -68,7 +68,7 @@ def get_search_reservation(request):
             messages.error(request, 'Reservation Not found')
             return redirect('reservations')
 
-
+@login_required
 def delete_reservation(request, reservation_code):
     """
     Delete an exsiting reservation
@@ -78,7 +78,7 @@ def delete_reservation(request, reservation_code):
     delete_reservation.delete()
     return redirect('client_reservations')
 
-
+@login_required
 def update_reservation(request, reservation_code):
     """
     Function to update an existing reservation
@@ -87,10 +87,19 @@ def update_reservation(request, reservation_code):
         reservation_code__exact=reservation_code)
     form = ReservationsForm(request.POST or None, instance=reservation)
     if form.is_valid():
+        time = request.POST.get('time_reservation')
+        date = request.POST.get('date_reservation')
+        form.instance.client = request.user
+        # Double reservation verification based in User logged, time and date.
+        duplicate_reservations = duplicate_reservations_verification(time, date, form)
+        if len(duplicate_reservations) > 0:
+            messages.error(request, 'There is an existing reservation')
+            return render(request, '../templates/search_reservation.html',
+                                   {'reservation': reservation})
         form.save()
         return render(request, '../templates/search_reservation.html',
-                               {'reservation': reservation})  
-    print(form.errors)
+                               {'reservation': reservation})
+
     return render(request, '../templates/update_reservation.html',
                            {'reservation': reservation, 'form': form})
 
@@ -100,10 +109,21 @@ def client_reservations(request):
     today = now.date()
     if request.user.is_authenticated:
         my_reservations = Reservations.objects.filter(client=request.user).filter(date_reservation__gte=today)
-        print(my_reservations)
         return render(request, '../templates/client_reservations.html',
-        {'my_reservations': my_reservations})
- 
+                               {'my_reservations': my_reservations})
+
+
+def duplicate_reservations_verification(time, date, form):
+    """
+    Use to verify existing reservations. If the return object is >0
+    a previous reservation is detected.
+    """
+    duplicate_reservations_number = Reservations.objects.filter(
+        Q(time_reservation=time),
+        Q(date_reservation=date),
+        Q(client=form.instance.client)
+    )
+    return duplicate_reservations_number
 
 def error_404(request, exception):
     return render(request, '../templates/error404.html')
