17bfc0c6801827a89366d44ab327576a3c549609
rfpotrero <raulf.potrero@gmail.com>
2022-10-19 13:16:28 +0000

    Fix style in template to make them responsive.
    Fixing style in templates to make them responsive. Mainly involved
    solving bootstrap issues. #40

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_details(self):
+        response = self.client.get('home')
+        self.assertEqual(response.status_code, 200)
