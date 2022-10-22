12673658167d901daa54a3506d6e1270e6a59b1a
rfpotrero <raulf.potrero@gmail.com>
2022-10-22 18:09:27 +0000

    Delete test.py since it won't be use

--- website/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_details(self):
-        response = self.client.get('home')
-        self.assertEqual(response.status_code, 200)
